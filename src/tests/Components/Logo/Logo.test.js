2a791a3ab95958c2d84af6cbf920f87b4ccc14d3
vitali-breida <vitaly2511@tut.by>
2021-04-18 13:54:17 +0300

    Logo component is covered by snapshot test

--- src/tests/Components/Logo/Logo.test.js
+++ src/tests/Components/Logo/Logo.test.js
@@ -1,12 +1,7 @@
-import renderer from "react-test-renderer";
 import Logo from "../../../app/Components/Logo/Logo";
 import { render } from "@testing-library/react";
 
-//afterEach(cleanup);
 it("renders correctly", () => {
-  //const tree = renderer.create(<Logo align="center" />).toJSON();
-  //expect(tree).toMatchSnapshot();
-
   const { asFragment } = render(<Logo align="center" />);
   expect(asFragment()).toMatchSnapshot();
 });
